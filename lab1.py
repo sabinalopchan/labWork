b9de52198f27916a35fdc6ca0b2d68b14f274ba9
sabinalopchan <sabinalopchan321@gmail.com>
2021-11-30 18:57:02 +0545

    lab1

---
+++ lab1.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+num3=int(input("Enter third number:"))
+result=num1+num2+num3
+# print(result)
+# print (f"the sum of number is {result}")
+print("the sum of number is {}.".format(result))
---
+++ lab2.py
@@ -0,0 +1,14 @@
+# h=int(input("enter height of triangle:"))
+# b=int(input("enter base of triangle:"))
+# area=(h*b)/2
+# print(f"the area of triangle is {area}.cm\u00b2")
+# print(f"the area of triangle is {}.".format(area)cm\u00b2")
+
+# l=int(input("Enter length of cube:"))
+# volume=l*l*l
+# print(f"the volume of cube is {volume}.cm\u00b3")
+
+
+
+
+
