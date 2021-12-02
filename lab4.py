df97975c97a3b73f01c6d2f3fa6c9274219eb451
sabinalopchan <sabinalopchan321@gmail.com>
2021-12-02 12:26:24 +0545

    assignment

--- lab4.py
+++ lab4.py
@@ -9,7 +9,7 @@ print (f"Sum of first ten even number is {sum}")
 
 # sum of first ten odd number
 sum=0
-# for num in range(0,11):
-#     if(num%2!=0):
-#         sum+=num
-# print (f"Sum of first ten odd number is {sum}")
+for num in range(0,11):
+    if(num%2!=0):
+        sum+=num
+print (f"Sum of first ten odd number is {sum}")
