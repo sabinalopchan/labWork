e7d224359ff6f929f105a8831393c397438d3fbb
sabinalopchan <sabinalopchan321@gmail.com>
2021-12-14 22:01:22 +0545

    day5

---
+++ day5/lab1.py
@@ -0,0 +1,9 @@
+# Write a Python program to find those numbers which are divisible by 7 and
+#  multiple of 5, between 1500 and 2700 (both included).
+
+a=[]
+for x in range(1500,2700):
+    if(x%7==0) and (x%5==0):
+        a.append(str(x))
+print (','.join(a))
+
---
+++ day5/lab2.py
@@ -0,0 +1,7 @@
+# Write a Python program to convert temperatures to and from celsius,
+#  fahrenheit.C = (5/9) * (F -32)
+
+f=float(input("enter the value in fahrenheit degree:"))
+c=(5/9)*(f-32)
+print(c)
+print(f"the value of {f} degree fahrenheit is {c} degr celcius")
